fe145703f6e26669f213f29e752e1823afa78b95
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-06-29 17:25:34 +0200

    add styles to overview component

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -9,7 +9,12 @@ export class OverviewComponent implements OnInit {
   accountBalance = 30659.45;
   pending = -19500.0;
   processed = 28750.0;
+  status: boolean = true;
   constructor() {}
 
   ngOnInit(): void {}
+  clickedTitle() {
+    console.log('dziala');
+    this.status = !this.status;
+  }
 }
