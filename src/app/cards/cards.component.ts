74d43ed911e7136be341ae0f731997f0ae4f2864
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-07-28 15:36:54 +0200

    change url in cards component

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,21 +12,21 @@ export class CardsComponent implements OnInit {
       number: '0000 1111 2222 3333',
       expiryDate: new Date(2025, 7),
       holderDetails: 'Adam Nowak',
-      url: 'url(./../../assets/images/cards/ship-640x438.jpg)',
+      url: 'url(assets/images/cards/ship-640x438.jpg)',
     },
     {
       name: 'Millenium',
       number: '1234 5678 9123 4567',
       expiryDate: new Date(2026, 9),
       holderDetails: 'Adam Nowak',
-      url: 'url(./../../assets/images/cards/shelby-640×350.jpg)',
+      url: 'url(assets/images/cards/shelby-640×350.jpg)',
     },
     {
       name: 'Santander',
       number: '9999 5555 3322 1231',
       expiryDate: new Date(2028, 11),
       holderDetails: 'Adam Nowak',
-      url: 'url(./../../assets/images/cards/plane-640×426.jpg)',
+      url: 'url(assets/images/cards/plane-640×426.jpg)',
     },
   ];
   constructor() {}
